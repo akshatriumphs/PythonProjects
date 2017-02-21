9f8bb4fe836d04f4e6b8a460739309c4c0ac9b25
akshat <akshat@gmai.com>
2017-02-21 04:38:12 -0500

    Python2

---
+++ arithmetic.py
@@ -0,0 +1,13 @@
+num1 = int(input('PLease enter the first integer: '))
+num2 = int(input('Please enter the second integer: '))
+sum = num1 + num2
+print ('The sum of ', num1,' and ', num2,' is ', sum)
+difference = num1 - num2
+print ('The difference of ', num1,' and ', num2,' is ', difference)
+product = num1 * num2
+print ('The product of ', num1,' and ', num2,' is ', product)
+quotient = num1/num2
+remainder = num1%num2
+print ('The quotient of ', num1,' and ', num2,' is ', quotient, ' with reamainder ', remainder)
+
+
